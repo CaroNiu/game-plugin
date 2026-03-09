091f32c3f0e8bbf7ca99e6f0daf1637e59bd78c5
JMS\caro.niu <caro.niu@jtexpress.com>
2026-03-09 15:37:07 +0800

    feat(core): 升级插件版本到 3.0.0
    - 更新 build.gradle.kts 中的版本号从 2.0.0 到 3.0.0
    - 更新 pluginConfiguration 中的版本号从 2.0.0 到 3.0.0
    - 在 plugin.xml 中添加 3.0.0 版本的更新说明
    - 新增球队排名查看功能
    - 优化比赛详情展示
    - 修复已知问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.caro"
-version = "2.0.0"
+version = "3.0.0"
 
 repositories {
     mavenCentral()
@@ -35,7 +35,7 @@ dependencies {
 intellijPlatform {
     pluginConfiguration {
         name = "NBA Live Score"
-        version = "2.0.0"
+        version = "3.0.0"
         
         ideaVersion {
             sinceBuild = "232"
